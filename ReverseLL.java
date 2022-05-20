70067d182b3fd0be08567bb7f354d0e907abed8e
arshmohd <irfan.arshad4@gmail.com>
2022-05-20 17:56:37 +0530

    Implemented the algos for all the 3 linkedList problems

--- ReverseLL.java
+++ ReverseLL.java
@@ -11,7 +11,7 @@
 public class ReverseLL {
   // Time O(N)
   // Space O(1)
-  // Did this code successfully run on Leetcode :
+  // Did this code successfully run on Leetcode : Yes
   // Any problem you faced while coding this :
   static ListNode head; // head of list
 
